e2be3812856c6f293e9a90997d01a82022ae5da9
lxh <519464203@qq.com>
2025-02-08 10:44:32 +0800

    忽略提交内容

---
+++ assets/post_diary_2016-2017_2016-3-3.md.D4PshTnd.lean.js
@@ -0,0 +1,11 @@
+import{_ as n,c as a,o,j as t}from "./chunks/framework.BVgKs9HZ.js";const _=JSON.parse('{"title":"雁来","description":"八九雁来天","frontmatter":{"title":"雁来","date":"2016-03-03T22:46:50.000Z","categories":"回忆","tags":["一日一记"],"description":"八九雁来天"},"headers":[],"relativePath":"post/diary/2016-2017/2016-3-3.md","filePath":"post/diary/2016-2017/2016-3-3.md"}'),r={name:"post/diary/2016-2017/2016-3-3.md"};function s(l, e, d, i, p, c){return o(),a("div",null,e[0]||(e[0]=[t("blockquote",null,[t("p",null,"济南果然是北方的火炉，还没有出正月，温度就达到了15度以上了，我只好褪去秋裤，换上单衣，春暖花开。看以前的日记，还能搜出些许，择一骚诗而展。")],-1),t("pre",null,[t("code",null,`	     春景
+     柳又棉吹,嵇土放翁旧梦;
+     春波桥上,了奏一曲重重;
+     素手慢拓,对酌浅饮,伊人婀娜俏影;
+   浮水向右,暖光攒动,恰似几撇惊鸿掠屏;
+
+     杳杳余晖,斜搓半山桃红;
+     琉璃瓦亭,静赏一日长空;
+     东来冬往,雁燕双飞,已到花暖时节;
+   女儿着裙,男儿着衫,真是一袭人间美景;
+`)],-1)]))}const m=n(r,[["render",s]]);export{_ as __pageData,m as default};
